813193616b626e3d661633f8f365f410ba377290
Eka Cahya Putra <ekacahyaps@gmail.com>
2022-12-20 04:29:42 +0700

    membuat struct user

---
+++ user/user.go
@@ -0,0 +1,95 @@
+package user
+
+import (
+	"database/sql"
+	"errors"
+	"log"
+)
+
+type User struct {
+	ID       int
+	Nama     string
+	Password string
+}
+
+type AuthMenu struct {
+	DB *sql.DB
+}
+
+// func NewAuthMenu() *AuthMenu {
+// 	cfg := config.ReadConfig()
+// 	conn := config.ConnectSQL(*cfg)
+// 	return &AuthMenu{DB: conn}
+// }
+
+func (am *AuthMenu) Duplicate(name string) bool {
+	res := am.DB.QueryRow("SELECT id_pegawai FROM pegawai where nama = ?", name)
+	var idExist int
+	err := res.Scan(&idExist)
+	if err != nil {
+		log.Println("Result scan error", err.Error())
+		return false
+	}
+	return true
+}
+
+func (am *AuthMenu) Register(newUser User) (bool, error) {
+	// menyiapakn query untuk insert
+	registerQry, err := am.DB.Prepare("INSERT INTO pegawai (nama, password) values (?,?)")
+	if err != nil {
+		log.Println("prepare insert user ", err.Error())
+		return false, errors.New("prepare statement insert user error")
+	}
+
+	if am.Duplicate(newUser.Nama) {
+		log.Println("duplicated information")
+		return false, errors.New("nama sudah digunakan")
+	}
+
+	// menjalankan query dengan parameter tertentu
+	res, err := registerQry.Exec(newUser.Nama, newUser.Password)
+	if err != nil {
+		log.Println("insert user ", err.Error())
+		return false, errors.New("insert user error")
+	}
+	// Cek berapa baris yang terpengaruh query diatas
+	affRows, err := res.RowsAffected()
+
+	if err != nil {
+		log.Println("after insert user ", err.Error())
+		return false, errors.New("error setelah insert")
+	}
+
+	if affRows <= 0 {
+		log.Println("no record affected")
+		return false, errors.New("no record")
+	}
+
+	return true, nil
+}
+
+func (am *AuthMenu) Login(nama string, password string) (User, error) {
+	loginQry, err := am.DB.Prepare("SELECT id_pegawai FROM pegawai WHERE nama = ? AND password = ?")
+	if err != nil {
+		log.Println("prepare insert user ", err.Error())
+		return User{}, errors.New("prepare statement insert user error")
+	}
+
+	row := loginQry.QueryRow(nama, password)
+
+	if row.Err() != nil {
+		log.Println("login query ", row.Err().Error())
+		return User{}, errors.New("tidak bisa login, data tidak ditemukan")
+	}
+	res := User{}
+	err = row.Scan(&res.ID)
+
+	if err != nil {
+		log.Println("after login query ", err.Error())
+		return User{}, errors.New("tidak bisa login, kesalahan setelah error")
+	}
+
+	res.Nama = nama
+
+	return res, nil
+}
