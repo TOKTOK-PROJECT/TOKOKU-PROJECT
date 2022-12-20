727e2c89f411af5c53af12313459ed4d1a573303
Eka Cahya Putra <ekacahyaps@gmail.com>
2022-12-20 15:16:14 +0700

    membuat method untuk menampilkan barang

--- barang/barang.go
+++ barang/barang.go
@@ -48,3 +48,64 @@ func (am *BarangMenu) Insert(newBarang Barang) (int, error) {
 
 	return int(id), nil
 }
+
+//Update db
+//  func (bm *BarangMenu) Update(id int, stok int) () {
+// 	stmt, e := bm.DB.Prepare("UPDATE barang SET stok=? where id=?")
+// 	ErrorCheck(e)
+
+// 	// execute
+// 	res, e := stmt.Exec("This is post five", "5")
+// 	ErrorCheck(e)
+
+// 	a, e := res.RowsAffected()
+// 	ErrorCheck(e)
+
+// 	fmt.Println(a)
+//  }
+
+// query all data
+func (bm *BarangMenu) Show() (barang2 []Barang) {
+	rows, e := bm.DB.Query(
+		`SELECT id_barang,
+		nama_barang,
+		deskripsi,
+		stok,
+		id_pegawai
+		FROM barang;`)
+
+	if e != nil {
+		log.Println(e)
+		return
+	}
+
+	barang2 = make([]Barang, 0)
+	for rows.Next() {
+		bar := Barang{}
+		rows.Scan(&bar.ID, &bar.Nama, &bar.Deskripsi, &bar.Stok, &bar.Owner)
+		barang2 = append(barang2, bar)
+	}
+	return barang2
+}
+
+// delete data
+//  func (bm *BarangMenu) Delete(id int) (int, error) {
+// 	stmt, e := bm.DB.Prepare("delete from posts where id=?")
+// 	ErrorCheck(e)
+
+// 	// delete 5th post
+// 	res, e := stmt.Exec("5")
+// 	ErrorCheck(e)
+
+// 	// affected rows
+// 	a, e := res.RowsAffected()
+// 	ErrorCheck(e)
+
+// 	fmt.Println(a) // 1
+//    }
+
+//    func ErrorCheck(err error) {
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+//    }
