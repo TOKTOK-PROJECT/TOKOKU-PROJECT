485620d980543ec3dbb2a0f669bbed61076f0aa8
Eka Cahya Putra <ekacahyaps@gmail.com>
2022-12-20 19:18:40 +0700

    menambahkan method hapus barang, edit deskripsi, update stok

--- barang/barang.go
+++ barang/barang.go
@@ -18,13 +18,14 @@ type BarangMenu struct {
 	DB *sql.DB
 }
 
-func (am *BarangMenu) Insert(newBarang Barang) (int, error) {
-	insertBarangQry, err := am.DB.Prepare("INSERT INTO barang (nama_barang, deskripsi, stok, id_pegawai) values (?,?,?,?)")
+func (bm *BarangMenu) Insert(newBarang Barang) (int, error) {
+	insertBarangQry, err := bm.DB.Prepare("INSERT INTO barang (nama_barang, deskripsi, stok, id_pegawai) values (?,?,?,?)")
 	if err != nil {
 		log.Println("prepare insert barang ", err.Error())
 		return 0, errors.New("prepare statement insert barang error")
 	}
 
+	// menjalankan query prepare
 	res, err := insertBarangQry.Exec(newBarang.Nama, newBarang.Deskripsi, newBarang.Stok, newBarang.Owner)
 
 	if err != nil {
@@ -32,6 +33,7 @@ func (am *BarangMenu) Insert(newBarang Barang) (int, error) {
 		return 0, errors.New("insert barang error")
 	}
 
+	// mengecek jumlah baris yang terpengaruh query diatas
 	affRows, err := res.RowsAffected()
 
 	if err != nil {
@@ -49,22 +51,6 @@ func (am *BarangMenu) Insert(newBarang Barang) (int, error) {
 	return int(id), nil
 }
 
-//Update db
-//  func (bm *BarangMenu) Update(id int, stok int) () {
-// 	stmt, e := bm.DB.Prepare("UPDATE barang SET stok=? where id=?")
-// 	ErrorCheck(e)
-
-// 	// execute
-// 	res, e := stmt.Exec("This is post five", "5")
-// 	ErrorCheck(e)
-
-// 	a, e := res.RowsAffected()
-// 	ErrorCheck(e)
-
-// 	fmt.Println(a)
-//  }
-
-// query all data
 func (bm *BarangMenu) Show() (barang2 []Barang) {
 	rows, e := bm.DB.Query(
 		`SELECT id_barang,
@@ -88,24 +74,95 @@ func (bm *BarangMenu) Show() (barang2 []Barang) {
 	return barang2
 }
 
-// delete data
-//  func (bm *BarangMenu) Delete(id int) (int, error) {
-// 	stmt, e := bm.DB.Prepare("delete from posts where id=?")
-// 	ErrorCheck(e)
+func (bm *BarangMenu) Delete(deleteBarang Barang) (bool, error) {
 
-// 	// delete 5th post
-// 	res, e := stmt.Exec("5")
-// 	ErrorCheck(e)
+	registerQry, err := bm.DB.Prepare("DELETE FROM barang WHERE id_barang=?")
+	if err != nil {
+		log.Println("prepare delete barang ", err.Error())
+		return false, errors.New("prepare statement delete barang error")
+	}
 
-// 	// affected rows
-// 	a, e := res.RowsAffected()
-// 	ErrorCheck(e)
+	// menjalankan query prepare
+	res, err := registerQry.Exec(deleteBarang.ID)
+	if err != nil {
+		log.Println("delete barang ", err.Error())
+		return false, errors.New("delete barang error")
+	}
 
-// 	fmt.Println(a) // 1
-//    }
+	// mengecek jumlah baris yang terpengaruh query diatas
+	affRows, err := res.RowsAffected()
 
-//    func ErrorCheck(err error) {
-// 	if err != nil {
-// 		panic(err.Error())
-// 	}
-//    }
+	if err != nil {
+		log.Println("after Delete barang ", err.Error())
+		return false, errors.New("error setelah delete barang")
+	}
+
+	if affRows <= 0 {
+		log.Println("no record affected")
+		return false, errors.New("no record")
+	}
+
+	return true, nil
+}
+
+func (bm *BarangMenu) Edit(editBarang Barang) (bool, error) {
+
+	editQry, err := bm.DB.Prepare("UPDATE barang set deskripsi=?  where id_barang= ?")
+	if err != nil {
+		log.Println("Update barang prepare", err.Error())
+		return false, errors.New("prepare Edit barang error")
+	}
+
+	// menjalankan query prepare
+	res, err := editQry.Exec(editBarang.Deskripsi, editBarang.ID)
+	if err != nil {
+		log.Println("Update barang", err.Error())
+		return false, errors.New("update barang error")
+	}
+
+	// mengecek jumlah baris yang terpengaruh query diatas
+	affRows, err := res.RowsAffected()
+
+	if err != nil {
+		log.Println("after Update Barang", err.Error())
+		return false, errors.New("after Update Barang error")
+	}
+
+	if affRows <= 0 {
+		log.Println("No record affected")
+		return true, errors.New("no record")
+	}
+
+	return true, nil
+}
+
+func (bm *BarangMenu) UpdateStok(updateBarang Barang) (bool, error) {
+
+	addQry, err := bm.DB.Prepare("UPDATE barang set stok=? where id_barang= ?")
+	if err != nil {
+		log.Println("Update stok barang prepare", err.Error())
+		return false, errors.New("prepare update stok barang error")
+	}
+
+	// menjalankan query prepare
+	res, err := addQry.Exec(updateBarang.Stok, updateBarang.ID)
+	if err != nil {
+		log.Println("Update barang", err.Error())
+		return false, errors.New("update barang error")
+	}
+
+	// mengecek jumlah baris yang terpengaruh query diatas
+	affRows, err := res.RowsAffected()
+
+	if err != nil {
+		log.Println("after Update Barang", err.Error())
+		return false, errors.New("after Update Barang error")
+	}
+
+	if affRows <= 0 {
+		log.Println("No record affected")
+		return true, errors.New("no record")
+	}
+
+	return true, nil
+}
