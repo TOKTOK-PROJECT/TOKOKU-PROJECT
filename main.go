851812e61085354675016a4cbfd07d0fc5504864
Eka Cahya Putra <ekacahyaps@gmail.com>
2022-12-20 15:17:39 +0700

    memperbaiki menu, mengganti scanln dengan bufio, serta menambah menu lihat barang

--- main.go
+++ main.go
@@ -17,27 +17,26 @@ func main() {
 	var barangMenu = barang.BarangMenu{DB: conn}
 	for inputMenu != 0 {
 		fmt.Println(" ")
-		fmt.Println("Selamat Datang di Aplikasi TOKOKU")
+		fmt.Println("-- Selamat Datang di Aplikasi TOKOKU --")
 		fmt.Println(" ")
-		fmt.Println("MENU")
-		fmt.Println("===================================")
+		fmt.Println("================= MENU =================")
 		fmt.Println("1. Register")
 		fmt.Println("2. Login")
 		fmt.Println("0. Exit")
-		fmt.Println("===================================")
+		fmt.Println("========================================")
 		fmt.Println("Silakan masukkan pilihan anda : ")
 		fmt.Scanln(&inputMenu)
 		if inputMenu == 1 {
 			var newUser user.User
 			in := bufio.NewReader(os.Stdin)
-			fmt.Print("Masukkan nama : ")
+			fmt.Print("Masukkan nama pegawai : ")
 			name, _ := in.ReadString('\n')
+			name = name[:len(name)-2]
 			newUser.Nama = name
-			// fmt.Scanln(&newUser.Nama)
 			fmt.Print("Masukkan password : ")
 			pass, _ := in.ReadString('\n')
+			pass = pass[:len(pass)-2]
 			newUser.Password = pass
-			// fmt.Scanln(&newUser.Password)
 			res, err := authMenu.Register(newUser)
 			if err != nil {
 				fmt.Println(err.Error())
@@ -49,18 +48,18 @@ func main() {
 			}
 		} else if inputMenu == 2 {
 			var inputNama, inputPassword string
-			// in := bufio.NewReader(os.Stdin)
+			in := bufio.NewReader(os.Stdin)
 			fmt.Println(" ")
-			fmt.Println("MENU LOGIN PEGAWAI")
-			fmt.Println("=======================")
-			fmt.Print("Masukkan nama : ")
-			// name, _ := in.ReadString('\n')
-			// inputNama = name
-			fmt.Scanln(&inputNama)
+			fmt.Println("==========MENU LOGIN PEGAWAI============")
+			fmt.Println(" ")
+			fmt.Print("Masukkan nama anda : ")
+			name, _ := in.ReadString('\n')
+			name = name[:len(name)-2]
+			inputNama = name
 			fmt.Print("Masukkan password : ")
-			// pass, _ := in.ReadString('\n')
-			// inputPassword = pass
-			fmt.Scanln(&inputPassword)
+			pass, _ := in.ReadString('\n')
+			pass = pass[:len(pass)-2]
+			inputPassword = pass
 			res, err := authMenu.Login(inputNama, inputPassword)
 			if err != nil {
 				fmt.Println(err.Error())
@@ -71,26 +70,35 @@ func main() {
 				loginMenu := 0
 				for isLogin {
 					fmt.Println(" ")
-					fmt.Println("INVENTORY TOKO")
-					fmt.Println("=======================")
+					fmt.Println("============ INVENTORY TOKO ============")
+					fmt.Println(" ")
 					fmt.Println("1. Tambah Barang")
+					fmt.Println("2. Lihat Barang")
 					fmt.Println("9. Logout")
 					fmt.Print("Masukkan pilihan anda : ")
 					fmt.Scanln(&loginMenu)
 					if loginMenu == 1 {
 						inputBarang := barang.Barang{}
+						in := bufio.NewReader(os.Stdin)
 						fmt.Print("Masukkan Nama Barang : ")
-						fmt.Scanln(&inputBarang.Nama)
+						name, _ := in.ReadString('\n')
+						name = name[:len(name)-2]
+						inputBarang.Nama = name
 						fmt.Print("Masukkan Deskripsi Barang : ")
-						fmt.Scanln(&inputBarang.Deskripsi)
+						desc, _ := in.ReadString('\n')
+						desc = desc[:len(desc)-2]
+						inputBarang.Deskripsi = desc
 						fmt.Print("Masukkan Jumlah (Stok) : ")
 						fmt.Scanln(&inputBarang.Stok)
+						inputBarang.Owner = res.ID
 						barRes, err := barangMenu.Insert(inputBarang)
 						if err != nil {
 							fmt.Println(err.Error())
 						}
 						inputBarang.ID = barRes
 						fmt.Println(inputBarang)
+					} else if loginMenu == 2 {
+						fmt.Println(barangMenu.Show())
 					} else if loginMenu == 9 {
 						isLogin = false
 					}
