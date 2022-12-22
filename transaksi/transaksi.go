a08901527e1a65435863a7c63054b0a6b2239444
Eka Cahya Putra <ekacahyaps@gmail.com>
2022-12-22 23:31:48 +0700

    memperbaiki method delete

--- transaksi/transaksi.go
+++ transaksi/transaksi.go
@@ -75,27 +75,53 @@ func (tm *TransaksiMenu) Show() (transaksi []Transaksi) {
 
 func (tm *TransaksiMenu) DeleteTransaksi(DeleteTransaksi Transaksi) (bool, error) {
 
+	deleteItemQry, err := tm.DB.Prepare("DELETE FROM item WHERE no_nota=?")
+	if err != nil {
+		log.Println("prepare delete transaksi ", err.Error())
+		return false, errors.New("prepare statement delete transaksi error")
+	}
+
 	deleteQry, err := tm.DB.Prepare("DELETE FROM transaksi WHERE no_nota=?")
 	if err != nil {
 		log.Println("prepare delete transaksi ", err.Error())
 		return false, errors.New("prepare statement delete transaksi error")
 	}
 
-	// menjalankan query prepare
-	res, err := deleteQry.Exec(DeleteTransaksi.NoNota)
+	// menjalankan query prepare delete item
+
+	res, err := deleteItemQry.Exec(DeleteTransaksi.NoNota)
+	if err != nil {
+		log.Println("delete Item ", err.Error())
+		return false, errors.New("delete Item error")
+	}
+	// Cek jumlah baris yang terpengaruh query diatas
+	affRows, err := res.RowsAffected()
+
+	if err != nil {
+		log.Println("after delete Item", err.Error())
+		return false, errors.New("error setelah delete Item")
+	}
+
+	if affRows <= 0 {
+		log.Println("no record affected")
+		return false, errors.New("no record")
+	}
+
+	// menjalankan query prepare delete transaksi
+	result, err := deleteQry.Exec(DeleteTransaksi.NoNota)
 	if err != nil {
 		log.Println("delete transaksi ", err.Error())
 		return false, errors.New("delete transaksi error")
 	}
 	// Cek jumlah baris yang terpengaruh query diatas
-	affRows, err := res.RowsAffected()
+	affeRows, err := result.RowsAffected()
 
 	if err != nil {
 		log.Println("after delete transaksi", err.Error())
 		return false, errors.New("error setelah delete transaksi")
 	}
 
-	if affRows <= 0 {
+	if affeRows <= 0 {
 		log.Println("no record affected")
 		return false, errors.New("no record")
 	}
